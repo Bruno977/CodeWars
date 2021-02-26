b3fb10aad4879cc42f07dec35d4bf007c4f72daa
Bruno Rodrigues <47644815+Bruno977@users.noreply.github.com>
2021-02-26 07:45:54 -0300

    Update splitString.js
    Add "_" odd letters.

--- splitStrings.js
+++ splitStrings.js
@@ -4,6 +4,10 @@ function solution(str){
    for(let i = 0; i < separateString.length; i++){
        array.push(separateString[i] + separateString[i+1])
        i++
+     if(separateString[i] === undefined){
+       array.pop()
+       array.push((separateString[i -1]) + "_")
+     }
    }
    console.log(array)
-}solution('abc')
+}solution('abcdefg')
