797e8640475afecdcbf8ca10c5f54f8b735cc16c
Bruno <bruno_151997@hotmail.com>
2021-03-02 21:16:04 -0300

    review this challenge

--- buggerPersistente.js
+++ buggerPersistente.js
@@ -1,3 +1,9 @@
 function persistence(num) {
-    //code me
-} persistence(399)
+    let calc = 0
+    let newNumber = String(num).split("")
+    calc = newNumber[0]
+    for (let i = 0; newNumber.length > 1; i++) {
+        calc *= newNumber[i + 1]
+        console.log(calc)
+    }
+} persistence(999)
